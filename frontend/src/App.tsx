1c97c873dc04560f5b558db507ee7da3495df793
fsobanski47 <fsobansi47@gmail.com>
2025-05-03 13:18:40 +0200

    init without electron

---
+++ frontend/src/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import AppLayout from "./components/app-layout";
+
+export default function App() {
+  return <AppLayout />;
+}
---
+++ frontend/src/components/app-layout/index.tsx
@@ -0,0 +1,61 @@
+import { IconButton } from "@mui/material";
+import { Home, Settings, AccountCircle } from "@mui/icons-material";
+
+function Sidebar() {
+  <div
+    style={{
+      backgroundColor: "#5aa9e6",
+      width: "50px", // Możesz dostosować szerokość
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between", // Równomiernie rozmieszcza przyciski
+      alignItems: "center",
+      paddingTop: "10px",
+    }}
+  >
+    <IconButton style={{ color: "white" }}>
+      <Home />
+    </IconButton>
+    <IconButton style={{ color: "white" }}>
+      <Settings />
+    </IconButton>
+    <IconButton style={{ color: "white" }}>
+      <AccountCircle />
+    </IconButton>
+  </div>;
+}
+
+export default function AppLayout() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#0b1e3f",
+        height: "100vh",
+        margin: 0,
+        fontFamily: "Roboto, sans-serif",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <header
+        style={{
+          backgroundColor: "#5aa9e6",
+          padding: "1rem",
+          textAlign: "center",
+        }}
+      >
+        <h1
+          style={{
+            color: "white",
+            letterSpacing: "0.5rem",
+            margin: 0,
+          }}
+        >
+          F R O N T D E S K
+        </h1>
+      </header>
+
+      <main style={{ flex: 1 }}></main>
+    </div>
+  );
+}
